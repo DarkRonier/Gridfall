9623734e86cb80d0707f7793a773feca1ffe77fc
Matias Brunaga <genaro.brunaga@fiuni.edu.py>
2025-10-13 23:20:34 -0300

    cambios en piece.py

--- game/piece.py
+++ game/piece.py
@@ -60,7 +60,7 @@ def crear_soldado(jugador):
 
 def crear_paladin(jugador):
     return Pieza(
-        nombre="Paladin", jugador=jugador, hp=30, atk=5, agi=5.5,
+        nombre="Paladin", jugador=jugador, hp=30, atk=5, agi=5.2,
         movimientos=[('rect', 4), ('diag', 3)],
         rango_ataque=[('rect', 1)],
         tipo_turno=1,
@@ -70,10 +70,10 @@ def crear_paladin(jugador):
 
 def crear_mago(jugador):
     return Pieza(
-        nombre="Mago", jugador=jugador, hp=12, atk=6, agi=5,
-        movimientos=[('steps', 2)],
-        rango_ataque=[('steps', (0, 3))],
-        tipo_turno=1,
+        nombre="Mago", jugador=jugador, hp=12, atk=7, agi=4.8,
+        movimientos=[('allsides', 2)],
+        rango_ataque=[('steps', (0, 4))],
+        tipo_turno=0,
         tipo_ataque='ranged',
         puede_saltar=False
     )
@@ -81,7 +81,7 @@ def crear_mago(jugador):
 def crear_dragon(jugador):
     return Pieza(
         nombre="Dragon", jugador=jugador, hp=40, atk=7, agi=4,
-        movimientos=[('allsides', 2)],
+        movimientos=[('allsides', 3)],
         rango_ataque=[('steps', (0, 3))],
         tipo_turno=1,
         tipo_ataque='ranged',
@@ -90,7 +90,7 @@ def crear_dragon(jugador):
 
 def crear_destructor(jugador):
     return Pieza(
-        nombre="Destructor", jugador=jugador, hp=50, atk=10, agi=4.5,
+        nombre="Destructor", jugador=jugador, hp=50, atk=10, agi=4.4,
         movimientos=[('allsides', 1)],
         rango_ataque=[('allsides', 1)],
         tipo_turno=1,
